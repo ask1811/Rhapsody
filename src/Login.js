ca802c69a59dfb203424c7effee8091723ec9b5a
Ashwin S Kuamar <ashwnskmr@gmail.com>
2022-12-05 21:11:31 +0530

    all

--- src/Login.js
+++ src/Login.js
@@ -6,10 +6,10 @@ function Login() {
   return (
     <div className="login">
       <img
-        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        src="rhapsodylogo.svg"
         alt=""
       />
-      <a href={accessUrl}>LOGIN TO SPOTIFY</a>
+      <a href={accessUrl}>LOGIN TO RHAPSODY</a>
     </div>
   );
 }
